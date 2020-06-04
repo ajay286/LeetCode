63b92b3eb067aa9982b02f88c92d5d0dfdad7571
Ajay Kumar <ajay_286@yahoo.com>
2020-06-04 10:01:25 -0400

    Create ReverseString.java

---
+++ June/ReverseString.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+/* Write a function that reverses a string. The input string is given as an array of characters char[].
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * You may assume all the characters consist of printable ascii characters.
+ * */
+
+public class ReverseString {
+
+	public void reverseString(char[] s) {
+		int lb = 0, ub = s.length - 1;
+
+		while (lb < ub) {
+			char tempHolder = s[lb];
+			s[lb] = s[ub];
+			s[ub] = tempHolder;
+			lb++;
+			ub--;
+		}
+
+	}
+
+	public static void main(String[] args) {
+		ReverseString reverseStr = new ReverseString();
+		char[] s = "hello".toCharArray();
+		System.out.println(Arrays.toString(s));
+		reverseStr.reverseString(s);
+		System.out.println(Arrays.toString(s));
+
+	}
+
+}
